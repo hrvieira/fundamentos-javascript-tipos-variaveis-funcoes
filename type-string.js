53b674a70e78807434c154f233c2895dd41473e1
hrvieira <luizhrvieira@outlook.com>
2023-03-20 12:53:32 -0300

    Adicionando conteúdo que foi trabalhado na inicialização do curso

---
+++ type-string.js
@@ -0,0 +1,25 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = 'senhaSuperSegura456!';
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Leonardo";
+console.log(citacao);
+
+// concatenação (+)
+
+console.log(citacao + meuNome);
+
+// Unicode
+
+const cifrao = '\u0024'
+const aMaiusculo = '\u0041'
+const tique = '\u2705'
+const hiragana = '\u3041'
+
+console.log(cifrao);
+console.log(aMaiusculo);
+console.log(tique);
+console.log(hiragana);
+
