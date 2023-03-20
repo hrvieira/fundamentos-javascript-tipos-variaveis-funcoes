4573836bcdca64615a4e6774ad768992566897d6
hrvieira <luizhrvieira@outlook.com>
2023-03-20 15:21:55 -0300

    adicionando mais alguns arquivos do estudo

---
+++ conversoes.js
@@ -0,0 +1,18 @@
+// tipo de dado
+// booleanos
+
+//conversão implícita
+const numero = 456;
+const numeroString = "456"; // pode ser usado aqui também como "= Number("456")" - não poder haver letras na string
+
+// Number()
+// String()
+
+console.log(numero + Number(numeroString));
+
+// console.log(numero == numeroString);
+// = atribuir a uma variável
+// == comparar  value
+// === comparar value e o tipo(string, numero, boolean)
+
+// conversão explícita
